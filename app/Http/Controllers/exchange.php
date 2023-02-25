8a93516f29182c6307655be3ea8518a6b579ddfd
Amr Saleh <52182013+AmrSaleh2234@users.noreply.github.com>
2023-02-25 13:21:15 +0200

    done problem products

--- app/Http/Controllers/exchange.php
+++ app/Http/Controllers/exchange.php
@@ -19,12 +19,13 @@ class exchange extends Controller
     public function products()
     {
         $products = order_customer::selectRaw(
-            'sum(order_customers.quantity)  order_quantity ,inventories.product_id  product_id ,products.name name ,sum(order_customers.price_after_discount) price'
+            'sum(order_customers.quantity)  order_quantity ,inventories.product_id  product_id ,products.name name ,sum(order_customers.price_after_discount) price ,invoice_customers.com_code  com_code'
 
         )
             ->join('inventories', 'order_customers.inventory_id', '=', 'inventories.id')
             ->groupBy('product_id', 'name')
             ->join('products', 'product_id', '=', 'products.id')
+            ->where("invoice_customers.com_code","=" ,$this->getAuthData('com_code'))
             ->get();
         return view('reports.products', compact('products'));
     }
@@ -43,11 +44,14 @@ class exchange extends Controller
 
         $products = order_customer::
         selectRaw(
-            ' sum(order_customers.quantity) order_quantity , products.name name ,inventories.product_id product_id ,sum(order_customers.price_after_discount) price ,invoice_customers.com_code  com_code')
+            ' sum(order_customers.quantity) order_quantity ,
+             products.name name ,inventories.product_id product_id ,
+             sum(order_customers.price_after_discount) price ,
+             invoice_customers.com_code  com_code')
             ->join('inventories', 'order_customers.inventory_id', '=', 'inventories.id')
             ->join('products', 'inventories.product_id', '=', 'products.id')->
             join('invoice_customers', 'order_customers.invoice_customer_id', '=', 'invoice_customers.id')
-                ->where("invoice_customers.com_code","=" ,$this->getAuthData('com_code'))->
+            ->where("invoice_customers.com_code","=" ,$this->getAuthData('com_code'))->
             whereDate("invoice_customers.created_at", ">=", $request->firstDate)
             ->whereDate("invoice_customers.created_at", "<=", $request->secondDate)
             ->groupBy('product_id', "name","com_code")
